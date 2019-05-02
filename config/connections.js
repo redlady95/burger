5a884ee3e3f2e3645e9601a5b58a5fe546c1e0d0
Laura Sedok <lsedok@abraauto.com>
2019-05-02 10:45:09 -0500

    hi

---
+++ config/connections.js
@@ -0,0 +1,57 @@
+// Set up MySQL connection.
+
+var mysql = require("mysql");
+
+//set up passwords with the dotenv package.
+
+require("dotenv").config();
+
+
+
+if (process.env.JAWSDB_URL) {
+
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+} else {
+
+//reminder to self, it will not work at port 3000
+
+  var connection = mysql.createConnection({
+
+    port: 3307,
+
+    host: "localhost",
+
+    user: "root",
+
+    password: "root",
+
+    database: "burgers_db"
+
+  });
+
+};
+
+
+
+// Make connection.
+
+connection.connect(function(err) {
+
+  if (err) {
+
+    console.error("error connecting: " + err.stack);
+
+    return;
+
+  }
+
+  console.log("connected as id " + connection.threadId);
+
+});
+
+
+
+// Export connection for our ORM to use.
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Whopper'), ('Big Mac'), ('Baconator');
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Cheesburger', true), ('Hamburger', false), ('California Burger', true);
